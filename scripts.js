002db387c69240f20684209aa27dc008cb99af2c
Alutta <Aluttamatambo5@gmail.com>
2024-11-10 12:07:12 +0200

    Added function subscribe & dispatch

--- scripts.js
+++ scripts.js
@@ -10,5 +10,17 @@ function createStore(reducer) {
     function dispatch(action) {
         state = reducer(state, action);
         listeners.forEach(listener => listener());
+    };
+
+    function subscribe(listener) {
+        listeners.push(listener)
+        return () => {
+            const index = listeners.indexOf(listener);
+            listeners.splice(index, 1);
+        };
     }
+
+    dispatch({});
+    return { getState, dispatch, subscribe };
+
 }
