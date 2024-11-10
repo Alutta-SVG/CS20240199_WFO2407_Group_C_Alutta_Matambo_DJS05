0dab5d2265b8da45b21c0906251bb55aec80b676
Alutta <Aluttamatambo5@gmail.com>
2024-11-10 12:04:17 +0200

    added function dispatch

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@ function createStore(reducer) {
     let state;
     const listeners =[];
 
-    
+
     fuction getState() {
         return state;
     };
+
+    function dispatch(action) {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    }
 }
